886c8d7f3147820652e6bc668ad35e035801e767
Marvin Gbei <131237945+ThoricFish@users.noreply.github.com>
2024-07-04 01:10:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const chalk = require('chalk');
 
 // Other
 global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.linkGroup = 'https://chat.whatsapp.com/HGC64KlI1M6HbEzTFB27C4'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "𝕿𝖍𝖔𝖗𝖎𝖈𝖋𝖎𝖘𝖍";
+global.owner = ['2347011765070'];
+global.premium = ['2347011765070'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "𝕿𝖍𝖔𝖗𝖎𝖈𝖋𝖎𝖘𝖍";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -33,7 +33,7 @@ global.mess = {
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
-    botAdmin: '`I am not an admin!`',
+    botAdmin: '`I am not an admin you idiot!`',
     owner: '`You are not my owner`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
